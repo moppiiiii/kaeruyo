c5038d2fee531f743842bc44585239d0e8f46065
moppiiiii <tnktmhssrc@gmail.com>
2024-12-28 17:27:47 +0900

    feat: dropdown-menu componentの作成

---
+++ src/components/ui/dropdown-menu/type.ts
@@ -0,0 +1,3 @@
+export type DropdownMenuProps = {
+  children: React.ReactNode;
+};
--- src/components/ui/header/Header.ui.tsx
+++ src/components/ui/header/Header.ui.tsx
@@ -1,9 +1,30 @@
 import GithubIcon from "../../../assets/icons/github-mark.svg";
 import SunIcon from "../../../assets/icons/sun.svg";
 import FrogIcon from "../../../assets/icons/frog.svg";
-import Dropdown from "../dropdown/Dropdown.ui";
+import DropdownMenu from "../dropdown-menu/DropdownMenu.ui";
+import { useState, useRef, useEffect } from "react";
+import ThemeMenu from "./_ui/theme-menu/ThemeMenu.ui";
 
 const Header: React.FC = () => {
+  const [isDropdownMenuOpen, setIsDropdownMenuOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <header className="text-gray-600 body-font shadow-md">
       <div className="flex justify-between p-5 items-center">
@@ -15,12 +36,22 @@ const Header: React.FC = () => {
         </div>
 
         <div className="flex items-center gap-4">
-          <button type="button">
-            <img className="w-6 h-6" src={SunIcon} alt="Sun" />
-          </button>
-          <Dropdown>
-            <div>Dropdown</div>
-          </Dropdown>
+          <div ref={dropdownRef} className="relative">
+            <button
+              className="flex items-center"
+              type="button"
+              onClick={() => setIsDropdownMenuOpen(!isDropdownMenuOpen)}
+            >
+              <img className="w-6 h-6" src={SunIcon} alt="Sun" />
+            </button>
+            {isDropdownMenuOpen && (
+              <div className="absolute end-full">
+                <DropdownMenu>
+                  <ThemeMenu />
+                </DropdownMenu>
+              </div>
+            )}
+          </div>
           <img className="w-6 h-6" src={GithubIcon} alt="Github" />
         </div>
       </div>
