c675ca3cf49cbf03c9cc092607de2fdac0fcd8dc
moppiiiii <tnktmhssrc@gmail.com>
2024-12-30 15:01:53 +0900

    fix: style

--- src/components/templates/food/Food.template.tsx
+++ src/components/templates/food/Food.template.tsx
@@ -22,7 +22,7 @@ import FoodIcon from "@/assets/icons/bowl-food-solid.svg";
 const FoodTemplate: React.FC = () => {
   return (
     <BaseLayout>
-      <div className="p-4 flex flex-col gap-4">
+      <div className="px-4 pt-4 pb-16 flex flex-col gap-4">
         <Card>
           <CardHeader>
             <CardTitle className="flex items-center gap-2">
@@ -38,7 +38,7 @@ const FoodTemplate: React.FC = () => {
             <CardDescription>2024/12/26 〜 2025/01/09</CardDescription>
           </CardHeader>
           <CardContent>
-            <div className="flex">
+            <div className="flex justify-center">
               <div className="flex flex-col justify-center items-center">
                 <div>
                   <Image
@@ -55,140 +55,140 @@ const FoodTemplate: React.FC = () => {
                 <p className="p-2">夜</p>
               </div>
               <ScrollArea className="w-auto whitespace-nowrap rounded-md">
-                <div className="flex w-max">
+                <div className="flex w-max justify-center">
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">26</p>
+                      <p className="p-2 text-center">26</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">27</p>
+                      <p className="p-2 text-center">27</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">28</p>
+                      <p className="p-2 text-center">28</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">29</p>
+                      <p className="p-2 text-center">29</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">30</p>
+                      <p className="p-2 text-center">30</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">31</p>
+                      <p className="p-2 text-centerr">31</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">1</p>
+                      <p className="p-2 text-center">1</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">2</p>
+                      <p className="p-2 text-center">2</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">3</p>
+                      <p className="p-2 text-center">3</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">4</p>
+                      <p className="p-2 text-center">4</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">5</p>
+                      <p className="p-2 text-center">5</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">6</p>
+                      <p className="p-2 text-center">6</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">7</p>
+                      <p className="p-2 text-center">7</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">8</p>
+                      <p className="p-2 text-center">8</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">9</p>
+                      <p className="p-2 text-center">9</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                 </div>
@@ -212,7 +212,7 @@ const FoodTemplate: React.FC = () => {
             <CardDescription>2024/12/26 〜 2025/01/09</CardDescription>
           </CardHeader>
           <CardContent>
-            <div className="flex">
+            <div className="flex justify-center">
               <div className="flex flex-col justify-center items-center">
                 <div>
                   <Image
@@ -229,140 +229,140 @@ const FoodTemplate: React.FC = () => {
                 <p className="p-2">夜</p>
               </div>
               <ScrollArea className="w-auto whitespace-nowrap rounded-md">
-                <div className="flex w-max">
+                <div className="flex w-max justify-center">
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">26</p>
+                      <p className="p-2 text-center">26</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">27</p>
+                      <p className="p-2 text-center">27</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">28</p>
+                      <p className="p-2 text-center">28</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">29</p>
+                      <p className="p-2 text-center">29</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">30</p>
+                      <p className="p-2 text-center">30</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">31</p>
+                      <p className="p-2 text-centerr">31</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">1</p>
+                      <p className="p-2 text-center">1</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">2</p>
+                      <p className="p-2 text-center">2</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">3</p>
+                      <p className="p-2 text-center">3</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">4</p>
+                      <p className="p-2 text-center">4</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">5</p>
+                      <p className="p-2 text-center">5</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">6</p>
+                      <p className="p-2 text-center">6</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">7</p>
+                      <p className="p-2 text-center">7</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">8</p>
+                      <p className="p-2 text-center">8</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                   <div className="shrink-0">
                     <div className="overflow-hidden">
-                      <p className="h-fit w-fit p-2">9</p>
+                      <p className="p-2 text-center">9</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">⭕️</p>
+                      <p className="p-2 text-center">⭕️</p>
                       <Separator />
-                      <p className="h-fit w-fit p-2">❌</p>
+                      <p className="p-2 text-center">❌</p>
                     </div>
                   </div>
                 </div>
