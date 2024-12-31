74d2143e37676da3a069c688573504de2c281bef
moppiiiii <tnktmhssrc@gmail.com>
2024-12-31 12:44:48 +0900

    feat: profile demo

--- src/components/templates/profile/Profile.template.tsx
+++ src/components/templates/profile/Profile.template.tsx
@@ -1,7 +1,33 @@
 import BaseLayout from "@/components/layouts/base/Base.layout";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar/Avatar.ui";
+import { Button } from "@/components/ui/button/Button.ui";
 
 const ProfileTemplate: React.FC = () => {
-  return <BaseLayout>ProfileTemplate</BaseLayout>;
+  return (
+    <BaseLayout>
+      <div className="flex flex-col gap-12 p-8">
+        <div className="flex items-center gap-6">
+          <Avatar className="size-20">
+            <AvatarImage
+              src="https://github.com/shadcn.png"
+              alt="avatar"
+              className="rounded-full"
+            />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col gap-2">
+            <p className="text-xl font-bold">Tomo</p>
+            <p className="text-sm text-gray-500">田中家</p>
+          </div>
+        </div>
+        <Button>ログアウト</Button>
+      </div>
+    </BaseLayout>
+  );
 };
 
 export default ProfileTemplate;
